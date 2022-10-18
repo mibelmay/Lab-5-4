a5da20ec70760cf125e942930df13e225f97307c
Stacy <zemcovannastasia11@gmail.com>
2022-10-18 23:30:52 +0500

    added method GetArray

--- Lab-5-4.cs
+++ Lab-5-4.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labs
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string[] numbers = GetArray();
+        }
+
+        public static string[] GetArray()
+        {
+            List<string> numbers = new List<string>();
+            Console.WriteLine("Введите массив. Чтобы закончить ввод, введите букву q");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!(input == "q"))
+                    numbers.Add(input);
+            }
+
+
+        }
+    }
+}
